be727b1af24929493ed89559841de8f7b51e4157
marcovittoriozacca <zaccamarco01@gmail.com>
2024-02-13 11:58:19 +0100

    feat: Header with searchbar and a button

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -7,14 +7,58 @@ export default {
 
 <template>
     <header>
-        <h1>Test Header</h1>
+        <div class="flex-header">
+            <h1>Boolflix</h1>
+    
+            <div>
+                <input type="text" name="searchbar" id="searchbar" placeholder="Inserisci il nome del film...">
+                <button type="button">Send</button>
+            </div>
+        </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../../assets/style/partials/mixins' as *;
+@use '../../assets/style/partials/variables' as *;
 
 header{
-    background-color: lightcoral;
+    background-color: rgb(38, 38, 38);
+    div.flex-header{
+        padding: 15px 0;
+        @include container-lg;
+        @include total-flex;
+        h1{
+            color: $red;
+            font-weight: 400;
+        }
+        > div{
+            display: flex;
+            column-gap: 10px;
+            input{
+                padding: 10px 20px 10px 5px;
+                border-radius: 5px;
+                border: none;
+                &:focus-visible{
+                    outline: none;
+                }
+            }
+            button{
+                text-transform: capitalize;
+                background-color: $red;
+                font-weight: bold;
+                border: none;
+                border-radius: 5px;
+                padding: 10px;
+                &:hover{
+                    cursor: pointer;
+                }
+                &:active{
+                    background-color: lightcoral;
+                }
+            }
+        }
+    }
 }
 
 </style>
